da16442452ef4cb4b15b5503686986a5d7e5c07c
Trần Anh Khôi <taikhoancuakhoi@gmail.com>
2019-03-24 21:01:46 +0700

    add sql

--- timkiemvieclam (1).sql
+++ timkiemvieclam (1).sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 14, 2019 at 03:25 PM
--- Server version: 10.1.36-MariaDB
--- PHP Version: 7.2.11
+-- Generation Time: Mar 24, 2019 at 03:01 PM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.3.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -56,9 +56,12 @@ INSERT INTO `tb_baituyen` (`id_baituyen`, `tieude`, `soluong`, `hinhthuc`, `gioi
 (1, 'tester', '10', 'full time', 0, '20', 1, 1, 'đại học', '2000USD', '20-05-2018', 'số 3 vũ trọng phụng', '', '0918272377', '2019-03-10 22:31:57', 1, 'asset/images/job.png'),
 (2, 'Kế toán', '1', 'full time', 1, '20', 2, 2, 'đại học', '2000USD', '20-9-2019', 'SỐ 30 hà thành', '', '0918277364 - Chị Hoa', '2019-03-10 22:32:13', 1, 'asset/images/job.png'),
 (4, 'gia sư toán cấp 2', '1', 'part-time', 1, '20', 4, 1, 'không', '20k/h', '20-2-2019', 'số 13 lê văn lương', '', 'chị hà - 01399998888', '2019-03-18 08:14:00', 3, ''),
-(5, 'gia sư toán cấp 2', '1', 'part-time', 1, '20', 4, 1, 'không', '20k/h', '20-2-2019', 'số 13 lê văn lương', '', 'chị hà - 01399998888', '2019-03-18 08:14:00', 3, 'asset/images/job.png'),
-(6, 'Designer ', '10', 'full time', 2, '20', 1, 1, 'không', '2.500.000 VND', '20-2-2019', 'Hà nội', '', 'Anh Hoàng - 0811283847', '2019-03-28 00:00:00', 1, ''),
-(7, 'Designer ', '10', 'full time', 2, '20', 1, 1, 'không', '2.500.000 VND', '20-2-2019', 'Hà nội', '', 'Anh Hoàng - 0811283847', '2019-03-28 00:00:00', 1, '');
+(5, 'gia sư toán cấp 2', '1', 'part-time', 1, '20', 4, 2, 'không', '20k/h', '20-2-2019', 'số 13 lê văn lương', '', 'chị hà - 01399998888', '2019-03-24 14:22:34', 3, 'asset/images/job.png'),
+(6, 'Designer ', '10', 'full time', 2, '20', 1, 1, 'không', '2.500.000 VND', '20-2-2019', 'Hà nội', '', 'Anh Hoàng - 0811283847', '2019-03-19 16:55:54', 1, 'asset/images/job.png'),
+(7, 'Designer ', '10', 'full time', 2, '20', 1, 1, 'không', '2.500.000 VND', '20-2-2019', 'Hà nội', '', 'Anh Hoàng - 0811283847', '2019-03-19 16:56:20', 1, 'asset/images/job.png'),
+(12, 'Gái ngành', '1', 'full time', 0, '18', 5, 2, '', '', '', '', '', '', '2019-03-19 16:56:20', 1, 'asset/images/job.png'),
+(13, 'Nam ngành', '2', 'full time', 0, '18', 1, 1, 'Đại học', '2000USD', '', 'Hà Nội', '', '', '2019-03-19 16:56:20', 1, 'asset/images/job.png'),
+(14, 'khôi đẹp zai', '1', 'fulltime', 0, '', 1, 1, '', '', '', '', '', '', '2019-03-19 16:56:20', 2, 'asset/images/job.png');
 
 -- --------------------------------------------------------
 
@@ -76,24 +79,11 @@ CREATE TABLE `tb_baiviet` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_bv_n`
---
-
-CREATE TABLE `tb_bv_n` (
-  `id` int(10) NOT NULL,
-  `id_bai` int(4) NOT NULL,
-  `id_nganh` int(5) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `tb_loaitk`
 --
 
 CREATE TABLE `tb_loaitk` (
   `id_loaitk` int(2) NOT NULL,
-  `quyen` int(2) NOT NULL,
   `ten` varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -101,10 +91,10 @@ CREATE TABLE `tb_loaitk` (
 -- Dumping data for table `tb_loaitk`
 --
 
-INSERT INTO `tb_loaitk` (`id_loaitk`, `quyen`, `ten`) VALUES
-(1, 0, 'admin'),
-(2, 1, 'user'),
-(3, 2, 'business');
+INSERT INTO `tb_loaitk` (`id_loaitk`, `ten`) VALUES
+(1, 'admin'),
+(2, 'user'),
+(3, 'business');
 
 -- --------------------------------------------------------
 
@@ -148,17 +138,22 @@ CREATE TABLE `tb_nhatd` (
   `id_nhatd` int(10) NOT NULL,
   `ten` varchar(50) NOT NULL,
   `mota` text NOT NULL,
-  `link` varchar(100) NOT NULL
+  `link` varchar(100) NOT NULL,
+  `sdt` varchar(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `diachi` varchar(100) NOT NULL,
+  `slogan` varchar(250) NOT NULL,
+  `id_tk` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `tb_nhatd`
 --
 
-INSERT INTO `tb_nhatd` (`id_nhatd`, `ten`, `mota`, `link`) VALUES
-(1, 'vingroup', '', ''),
-(2, 'dingtea', '', ''),
-(3, 'trung tâm gia sư Ánh Mai', '', '');
+INSERT INTO `tb_nhatd` (`id_nhatd`, `ten`, `mota`, `link`, `sdt`, `email`, `diachi`, `slogan`, `id_tk`) VALUES
+(1, 'vingroup', '', '', '', '', '', '', 1),
+(2, 'dingtea', '', '', '', '', '', '', 2),
+(3, 'trung tâm gia sư Ánh Mai', '', '', '', '', '', '', 3);
 
 -- --------------------------------------------------------
 
@@ -172,6 +167,29 @@ CREATE TABLE `tb_quangcao` (
   `noidung` text NOT NULL,
   `link` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_taikhoan`
+--
+
+CREATE TABLE `tb_taikhoan` (
+  `id_tk` int(11) NOT NULL,
+  `ten_tk` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `id_loaitk` int(2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tb_taikhoan`
+--
+
+INSERT INTO `tb_taikhoan` (`id_tk`, `ten_tk`, `password`, `id_loaitk`) VALUES
+(1, 'vin', '123', 3),
+(2, 'dingtea', '123', 3),
+(3, 'giasu', '123', 3),
+(4, 'khoi', '123', 2);
 
 -- --------------------------------------------------------
 
@@ -212,31 +230,26 @@ CREATE TABLE `tb_thanhvien` (
   `slogan` varchar(100) DEFAULT NULL,
   `id_thanhpho` int(4) DEFAULT NULL,
   `email` varchar(100) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `id_loaitk` int(2) DEFAULT NULL
+  `id_tk` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `tb_thanhvien`
 --
 
-INSERT INTO `tb_thanhvien` (`id_thanhvien`, `ten`, `diachi`, `sdt`, `gioitinh`, `bangcap`, `loai`, `ngoaingu`, `anh`, `kinhnghiem`, `slogan`, `id_thanhpho`, `email`, `password`, `id_loaitk`) VALUES
-(1, 'khôi', 'số 3 ngõ 39 lĩnh nam', '0916712266', 0, 'không', 0, 0, '', 'không', 'sống để cống hiến', 1, 'khoi@gmail.com', '1234', 2),
-(2, 'hoang', NULL, '0917672287', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'hoang@gmail.com', '123456', NULL),
-(3, 'admin', NULL, '01223345821', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'admin@gmail.com', '1234', NULL),
-(4, 'công', NULL, '01223345821', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'cong@gmail.com', '1234', NULL),
-(5, 'trung', NULL, '01223345821', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'trung@gmail.com', '1234', NULL);
+INSERT INTO `tb_thanhvien` (`id_thanhvien`, `ten`, `diachi`, `sdt`, `gioitinh`, `bangcap`, `loai`, `ngoaingu`, `anh`, `kinhnghiem`, `slogan`, `id_thanhpho`, `email`, `id_tk`) VALUES
+(1, 'Trần Anh Khôi', NULL, '0942755384', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'khoi@gmail.com', 4);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_tv_n`
+-- Table structure for table `tb_tv_ntd`
 --
 
-CREATE TABLE `tb_tv_n` (
-  `id` int(10) NOT NULL,
-  `id_nganh` int(5) NOT NULL,
-  `id_thanhvien` int(4) NOT NULL
+CREATE TABLE `tb_tv_ntd` (
+  `id` int(11) NOT NULL,
+  `id_thanhvien` int(11) DEFAULT NULL,
+  `id_nhatd` int(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -256,15 +269,8 @@ ALTER TABLE `tb_baituyen`
 -- Indexes for table `tb_baiviet`
 --
 ALTER TABLE `tb_baiviet`
-  ADD PRIMARY KEY (`id_bai`);
-
---
--- Indexes for table `tb_bv_n`
---
-ALTER TABLE `tb_bv_n`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_bai_FK` (`id_bai`),
-  ADD KEY `id_nganh_FK` (`id_nganh`) USING BTREE;
+  ADD PRIMARY KEY (`id_bai`),
+  ADD KEY `id_nganh` (`id_nganh`);
 
 --
 -- Indexes for table `tb_loaitk`
@@ -282,13 +288,21 @@ ALTER TABLE `tb_nganh`
 -- Indexes for table `tb_nhatd`
 --
 ALTER TABLE `tb_nhatd`
-  ADD PRIMARY KEY (`id_nhatd`);
+  ADD PRIMARY KEY (`id_nhatd`),
+  ADD KEY `id_ltk` (`id_tk`);
 
 --
 -- Indexes for table `tb_quangcao`
 --
 ALTER TABLE `tb_quangcao`
   ADD PRIMARY KEY (`id_qc`);
+
+--
+-- Indexes for table `tb_taikhoan`
+--
+ALTER TABLE `tb_taikhoan`
+  ADD PRIMARY KEY (`id_tk`),
+  ADD KEY `id_loaitk` (`id_loaitk`);
 
 --
 -- Indexes for table `tb_thanhpho`
@@ -302,15 +316,15 @@ ALTER TABLE `tb_thanhpho`
 ALTER TABLE `tb_thanhvien`
   ADD PRIMARY KEY (`id_thanhvien`),
   ADD KEY `id_tp_FK` (`id_thanhpho`),
-  ADD KEY `id_loaitk_tv` (`id_loaitk`) USING BTREE;
+  ADD KEY `id_tk` (`id_tk`);
 
 --
--- Indexes for table `tb_tv_n`
+-- Indexes for table `tb_tv_ntd`
 --
-ALTER TABLE `tb_tv_n`
+ALTER TABLE `tb_tv_ntd`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `id_nganh` (`id_nganh`),
-  ADD KEY `id_thanhvien` (`id_thanhvien`);
+  ADD KEY `idtv` (`id_thanhvien`),
+  ADD KEY `idntd` (`id_nhatd`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -320,19 +334,13 @@ ALTER TABLE `tb_tv_n`
 -- AUTO_INCREMENT for table `tb_baituyen`
 --
 ALTER TABLE `tb_baituyen`
-  MODIFY `id_baituyen` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_baituyen` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `tb_baiviet`
 --
 ALTER TABLE `tb_baiviet`
   MODIFY `id_bai` int(4) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `tb_bv_n`
---
-ALTER TABLE `tb_bv_n`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `tb_loaitk`
@@ -350,13 +358,19 @@ ALTER TABLE `tb_nganh`
 -- AUTO_INCREMENT for table `tb_nhatd`
 --
 ALTER TABLE `tb_nhatd`
-  MODIFY `id_nhatd` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_nhatd` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `tb_quangcao`
 --
 ALTER TABLE `tb_quangcao`
   MODIFY `id_qc` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tb_taikhoan`
+--
+ALTER TABLE `tb_taikhoan`
+  MODIFY `id_tk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `tb_thanhpho`
@@ -368,13 +382,13 @@ ALTER TABLE `tb_thanhpho`
 -- AUTO_INCREMENT for table `tb_thanhvien`
 --
 ALTER TABLE `tb_thanhvien`
-  MODIFY `id_thanhvien` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_thanhvien` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `tb_tv_n`
+-- AUTO_INCREMENT for table `tb_tv_ntd`
 --
-ALTER TABLE `tb_tv_n`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `tb_tv_ntd`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
@@ -389,24 +403,36 @@ ALTER TABLE `tb_baituyen`
   ADD CONSTRAINT `id_thanhpho_fk` FOREIGN KEY (`id_thanhpho`) REFERENCES `tb_thanhpho` (`id_thanhpho`);
 
 --
--- Constraints for table `tb_bv_n`
+-- Constraints for table `tb_baiviet`
 --
-ALTER TABLE `tb_bv_n`
-  ADD CONSTRAINT `id_bai_FK` FOREIGN KEY (`id_bai`) REFERENCES `tb_baiviet` (`id_bai`);
+ALTER TABLE `tb_baiviet`
+  ADD CONSTRAINT `id_nganh_bv` FOREIGN KEY (`id_nganh`) REFERENCES `tb_nganh` (`id_nganh`);
+
+--
+-- Constraints for table `tb_nhatd`
+--
+ALTER TABLE `tb_nhatd`
+  ADD CONSTRAINT `id_ltk` FOREIGN KEY (`id_tk`) REFERENCES `tb_taikhoan` (`id_tk`);
+
+--
+-- Constraints for table `tb_taikhoan`
+--
+ALTER TABLE `tb_taikhoan`
+  ADD CONSTRAINT `id_tk_loaitk` FOREIGN KEY (`id_loaitk`) REFERENCES `tb_loaitk` (`id_loaitk`);
 
 --
 -- Constraints for table `tb_thanhvien`
 --
 ALTER TABLE `tb_thanhvien`
   ADD CONSTRAINT `id_tp_FK` FOREIGN KEY (`id_thanhpho`) REFERENCES `tb_thanhpho` (`id_thanhpho`),
-  ADD CONSTRAINT `id_tv_ltk` FOREIGN KEY (`id_loaitk`) REFERENCES `tb_loaitk` (`id_loaitk`);
+  ADD CONSTRAINT `id_tv_tk` FOREIGN KEY (`id_tk`) REFERENCES `tb_taikhoan` (`id_tk`);
 
 --
--- Constraints for table `tb_tv_n`
+-- Constraints for table `tb_tv_ntd`
 --
-ALTER TABLE `tb_tv_n`
-  ADD CONSTRAINT `id_nganh` FOREIGN KEY (`id_nganh`) REFERENCES `tb_nganh` (`id_nganh`),
-  ADD CONSTRAINT `id_thanhvien` FOREIGN KEY (`id_thanhvien`) REFERENCES `tb_thanhvien` (`id_thanhvien`);
+ALTER TABLE `tb_tv_ntd`
+  ADD CONSTRAINT `idntd` FOREIGN KEY (`id_nhatd`) REFERENCES `tb_nhatd` (`id_nhatd`),
+  ADD CONSTRAINT `idtv` FOREIGN KEY (`id_thanhvien`) REFERENCES `tb_thanhvien` (`id_thanhvien`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
