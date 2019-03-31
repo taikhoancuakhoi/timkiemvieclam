28be24d1fc1232ff1d5d9049f0907331a5656656
trungcong1999 <khongnhomatkhau99@gmail.com>
2019-03-31 21:20:54 +0700

    add

--- project_timvieclam/view/user/user-job-sent.php
+++ project_timvieclam/view/user/user-job-sent.php
@@ -1,57 +1,22 @@
-<div id="page-wrapper" >
-            <div id="page-inner">
-                <div class="row">
-                    <div class="col-md-12">
-                     
-                    </div>
-                </div>              
-                 <!-- /. ROW  -->
-                 
-                
-                 <!-- /. ROW  -->
-                            
-                    <div class="row">
-                <div class="col-md-12">
-                    <!-- Advanced Tables -->
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                             <h4>Việc làm đã ứng tuyển</h4>
-                        </div>
-                        <div class="panel-body">
-                            <div class="table-responsive">
-                                <table class="table table-striped table-bordered table-hover" id="dataTables-example10">
-                                    <thead>
-                                        <tr>
-                                            <th>Tiêu đề công việc</th>
-                                            <th>Công ty</th>
-                                            <th>Ngày nộp</th>
-                                            
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr class="odd gradeX">
-                                            <td>Trident</td>
-                                            <td>vingroup</td>
-                                            <td>20-11-2019</td>
-                                            
-                                        </tr>
-                                       
-                                    </tbody>
-                                </table>
-                            </div>
-                            
-                        </div>
-                    </div>
-                    <!--End Advanced Tables -->
-                </div>
-            </div>
-           
-                            
-                        <!-- </div> -->
-                        
-         </div>
+<div class="job-new">
+<h4 style="color:#1F568B;font-weight: bold">Việc làm đã ứng tuyển <span style="font-size: 13px;">(click vào tiêu đề để xem bài)</span></h4>
+<table class="table table-bordered">
+	<tr>
+		<td style="color:#1F568B;font-weight: bold ">Tiêu đề công việc</td>
+		<td style="color:#1F568B;font-weight: bold ">Công ty</td>
+		<td style="color:#1F568B;font-weight: bold ">Ngày nộp</td>
+	</tr>
+	<?php 
+		foreach ($data as $key => $value) {
+	?>
+	<tr>
+		<td><a href="?controller=job&action=detail&id=<?php echo $value['id_baituyen']; ?>"><?php echo ucwords($value['tieude']); ?></a></td>
+		<td><?php echo $value['ten'] ?></td>
+		<td><?php echo date('d-m-Y',strtotime($value['thoigian'])); ?></td>
+	</tr>
 
-             <!-- /. PAGE INNER  -->
-            </div>
-         <!-- /. PAGE WRAPPER  -->
-        </div>
+	<?php	}?>
+	
+</table>
+</div>
+<!-- <?php echo "<pre>";print_r($data); ?> -->
---
+++ project_timvieclam/view/user/user-job-sent1.php
@@ -0,0 +1,57 @@
+<div id="page-wrapper" >
+            <div id="page-inner">
+                <div class="row">
+                    <div class="col-md-12">
+                     
+                    </div>
+                </div>              
+                 <!-- /. ROW  -->
+                 
+                
+                 <!-- /. ROW  -->
+                            
+                    <div class="row">
+                <div class="col-md-12">
+                    <!-- Advanced Tables -->
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                             <h4>Việc làm đã ứng tuyển</h4>
+                        </div>
+                        <div class="panel-body">
+                            <div class="table-responsive">
+                                <table class="table table-striped table-bordered table-hover" id="dataTables-example10">
+                                    <thead>
+                                        <tr>
+                                            <th>Tiêu đề công việc</th>
+                                            <th>Công ty</th>
+                                            <th>Ngày nộp</th>
+                                            
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr class="odd gradeX">
+                                            <td>Trident</td>
+                                            <td>vingroup</td>
+                                            <td>20-11-2019</td>
+                                            
+                                        </tr>
+                                       
+                                    </tbody>
+                                </table>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <!--End Advanced Tables -->
+                </div>
+            </div>
+           
+                            
+                        <!-- </div> -->
+                        
+         </div>
+
+             <!-- /. PAGE INNER  -->
+            </div>
+         <!-- /. PAGE WRAPPER  -->
+        </div>
