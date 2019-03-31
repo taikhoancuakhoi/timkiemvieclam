fa0a055e9bd863efb02bf641ef561acdd3408a65
trungcong1999 <khongnhomatkhau99@gmail.com>
2019-03-31 20:41:54 +0700

    login1

---
+++ project_timvieclam/view/user/login1.php
@@ -0,0 +1,47 @@
+ <div class="promo-block">
+            <div class="container">
+                <div class="margin-b-40">
+                </div>
+                                    
+            </div>
+        </div>
+        <br>
+        <div class="container">
+            <div class="row">
+    <div class="col-md-4 col-md-offset-4" style="box-shadow: 1px 1px 1px 1px #ccc;">
+      <br>
+      <!-- Nav tabs -->
+      <h3 style="text-align: center;">Đăng nhập thành viên</h3>
+      
+      
+      <div class="tab">
+       
+        <div class="tab" >
+          <br>  
+          <form action="" method="POST">
+          <fieldset>
+            <div class="form-group">
+              <div class="right">
+                <b>Tài khoản</b>
+                <input class="form-control " placeholder="Tài khoản" type="text">
+              </div>
+            </div>
+            <div class="form-group">
+              <div class="right">
+                <b>Mật khẩu</b>
+                <input class="form-control" placeholder="Mật khẩu" type="password">
+              </div>
+            </div>
+
+          </fieldset>
+      </form>
+          <br>
+          <div class=" text-center">
+            <button class="btn btn-primary btn-danger"><i class="fa fa-user"></i> Đăng nhập</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+        </div>
+           <br>
