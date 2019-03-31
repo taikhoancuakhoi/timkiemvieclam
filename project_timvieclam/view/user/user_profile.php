0e65b497cbc3a08c4e22caf3f26ea339e10b66f0
trungcong1999 <khongnhomatkhau99@gmail.com>
2019-03-31 21:17:18 +0700

    add

--- project_timvieclam/view/user/user_profile.php
+++ project_timvieclam/view/user/user_profile.php
@@ -1,16 +1,59 @@
-<div class="job-new">
-	<h4>Việc làm đã ứng tuyển mới nhất</h4>
+	<!-- việc làm đã lưu -->
 
-	<table class="table table-bordered">
-		<tr>
-			<td>Tiêu đề công việc</td>
-			<td>Công ty</td>
-			<td>Ngày nộp</td>
-		</tr>
-		<tr>
-			<td>rể</td>
-			<td>ể</td>
-			<td>ê</td>
-		</tr>
-	</table>
-</div>
+<div id="page-wrapper" >
+            <div id="page-inner">
+                <div class="row">
+                    <div class="col-md-12">
+                     
+                    </div>
+                </div>              
+                 <!-- /. ROW  -->
+                 
+                
+                 <!-- /. ROW  -->
+                            
+                    <div class="row">
+                <div class="col-md-12">
+                    <!-- Advanced Tables -->
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                             <h4>Việc làm đã lưu</h4>
+                        </div>
+                        <div class="panel-body">
+                            <div class="table-responsive">
+                                <table class="table table-striped table-bordered table-hover" id="dataTables-example9">
+                                    <thead>
+                                        <tr>
+                                            <th>Tiêu đề công việc</th>
+                                            <th>Công ty</th>
+                                            <th>Ngày nộp</th>
+                                            
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr class="odd gradeX">
+                                            <td>Trident</td>
+                                            <td>vingroup</td>
+                                            <td>20-11-2019</td>
+                                            
+                                        </tr>
+                                       
+                                    </tbody>
+                                </table>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <!--End Advanced Tables -->
+                </div>
+            </div>
+           
+                            
+                        <!-- </div> -->
+                        
+         </div>
+
+             <!-- /. PAGE INNER  -->
+            </div>
+         <!-- /. PAGE WRAPPER  -->
+        </div>
